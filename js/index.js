feea46131900c7d9aea3b3574c3347cde78908f4
ThatDiegoGuy <113506360+ThatDiegoGuy@users.noreply.github.com>
2023-03-30 12:54:24 +0200

    Updated

--- js/index.js
+++ js/index.js
@@ -13,7 +13,8 @@ const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
 //Counter character
 const counterSpeech = document.getElementById("counterSpeech");
 const counterAudio = document.getElementById("counterAudio");
-const counterPortrait = document.getElementById("counterCharacter");
+const counterAmelia = document.getElementById("counterAmelia");
+const counterBob = document.getElementById("counterBob");
 
 //Inventory
 const inventory = [];
@@ -28,34 +29,42 @@ gameWindow.onclick = function (e) {
 
     console.log(e.target.id);
 
-    switch (e.target.id){
-        case "character1":
-            if (checkItem("drugs")){
+    switch (e.target.id) {
+        case "character2": //Bob
+            if (checkItem("drugs")) {
                 showMessage(mainCharacterSpeech, characterAudio, "I have the drugs. Where is my money?");
-            }else {
-                showMessage(mainCharacterSpeech, characterAudio, "I think they want something... <br>What about that lady there? Maybe they know.")
+                setTimeout(function () { counterBob.style.opacity = 1;}, 4 * sec);
+                setTimeout(showMessage, 2 * sec, counterSpeech, counterAudio, "You got the drugs, he?<br>I will count it, I don't trust you." );
+                setTimeout(showMessage, 4 * sec, mainCharacterSpeech, characterAudio, "Alright, sounds good.");
+                hideMessage(mainCharacterSpeech, counterSpeech);
+                setTimeout(showMessage, 8 * sec, counterAudio, counterSpeech, "You got a deal!<br>Don't come back here again, comrade.");
+            } else {
+                setTimeout(showMessage, 0.5 * sec, mainCharacterSpeech, characterAudio, "Good day, lad!");
+                setTimeout(function() {counterBob.style.opacity = 1;}, 2 * sec);
+                setTimeout(showMessage, 5 * sec, counterSpeech, counterAudio, "Good day, son. What are you looking for?<br>Maybe some tea?");
+                setTimeout(showMessage, 9 * sec, mainCharacterSpeech, characterAudio, "I would like some of that white tea<br>if you know what I mean...");
             }
             break;
-        case "character2":
+        case "character1":  //Amelia
             showMessage(mainCharacterSpeech, characterAudio, "Greeting, ma'am!<br>What do you have for me?")
             break;
-            default:
+        default:
             hideMessage(mainCharacterSpeech, characterAudio);
             hideMessage(counterSpeech, counterAudio);
-            break; 
+            break;
     }
 }
 
-function showMessage(targetBalloon, targetSound, message){
+function showMessage(targetBalloon, targetSound, message) {
     targetSound.play();
-    targetBalloon.style.opacity = "1";
-    targetBalloon.innnerHTML = message;
+    targetBalloon.style.opacity = 1;
+    targetBalloon.innerHTML = message;
     setTimeout(hideMessage, 4 * sec, targetBalloon, targetSound);
 }
 
 function hideMessage(targetBalloon, targetSound) {
     targetSound.pause();
-    targetBalloon.style.opacity = "0";
+    targetBalloon.style.opacity = 0;
     targetBalloon.innerHTML = "...";
 }
 
@@ -73,7 +82,7 @@ function checkItem(item) {
 
 function showItem(toDoValue) {
     //Make a list item from scratch and store it in a variable
-    let listItem = document.createElement("li");
+    let listItem = document.createElement(li);
 
     //fill that list item with value of inputfield
     listItem.appendChild(document.createTextNode(toDoValue));
