d4029452352c3c54dc842cbe7bcb89229ef1b34b
ThatDiegoGuy <113506360+ThatDiegoGuy@users.noreply.github.com>
2023-04-05 10:42:31 +0200

    yes

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const counterAmelia = document.getElementById("counterAmelia");
 const counterBob = document.getElementById("counterBob");
 
 //Inventory
-const inventory = [];
+let inventory = [];
 const inventoryList = document.getElementById("inventoryList");
 
 gameWindow.onclick = function (e) {
@@ -43,10 +43,22 @@ gameWindow.onclick = function (e) {
                 setTimeout(function() {counterBob.style.opacity = 1;}, 2 * sec);
                 setTimeout(showMessage, 5 * sec, counterSpeech, counterAudio, "Good day, son. What are you looking for?<br>Maybe some tea?");
                 setTimeout(showMessage, 9 * sec, mainCharacterSpeech, characterAudio, "I would like some of that white tea<br>if you know what I mean...");
+                setTimeout(function() {counterBob.style.opacity = 0;}, 2 * sec);
             }
             break;
         case "character1":  //Amelia
-            showMessage(mainCharacterSpeech, characterAudio, "Greeting, ma'am!<br>What do you have for me?")
+        if (checkItem("drugs")){
+            setTimeout(function(){counterAmelia.style.opacity = 1;}, 2 * sec);
+            setTimeout(showMessage, 0.5 * sec, counterSpeech,counterAudio, "Hello, love! Would you like...<br>What are you doing here?");
+            setTimeout(showMessage, 3 * sec, mainCharacterSpeech, characterAudio, "I want to buy more of your drugs.");
+            setTimeout(showMessage, 5 * sec, counterSpeech, counterAudio, "I will get my batch next Tuesday.<br>Now, get lost!");
+        }else{
+            setTimeout(showMessage, 0.5 * sec, mainCharacterSpeech, characterAudio, "Greetings, ma'am!");
+            setTimeout(function() {counterAmelia.style.opacity = 1;}, 2 * sec);
+            setTimeout(showMessage, 2 * sec, counterAmelia, counterSpeech, "Hello, love! Would you like to buy something?<br>Our tea is the best in town!");
+            getItem("drugs");
+            setTimeout(function() {counterAmelia.style.opacity = 0;}, 6 * sec);
+        }
             break;
         default:
             hideMessage(mainCharacterSpeech, characterAudio);
@@ -68,10 +80,10 @@ function hideMessage(targetBalloon, targetSound) {
     targetBalloon.innerHTML = "...";
 }
 
-function getItem(item) {
-    if (!checkItem(item)) {
-        inventory.push(item);
-        showItem(item);
+function getItem(itemName, itemId) {
+    if (!checkItem(itemName)) {
+        inventory.push(itemName);
+        showItem(itemName, itemId);
     }
 
 }
@@ -80,12 +92,14 @@ function checkItem(item) {
     return inventory.includes(item);
 }
 
-function showItem(toDoValue) {
+function showItem(itemName, itemId) {
     //Make a list item from scratch and store it in a variable
-    let listItem = document.createElement(li);
+    let listItem = document.createElement("li");
+    
+    listItem.id = itemId;
 
     //fill that list item with value of inputfield
-    listItem.appendChild(document.createTextNode(toDoValue));
+    listItem.appendChild(document.createTextNode(itemName));
 
     //find UL with id todoContainer and attach the list item to it.
     inventoryList.appendChild(listItem);
